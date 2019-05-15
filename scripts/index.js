c34bca926dca95db407082ee8ed2d321d04951ea
bigeyestoad <rcnavarrop@gmail.com>
2019-05-15 09:52:20 -0500

    portfolio style done

--- scripts/index.js
+++ scripts/index.js
@@ -44,15 +44,11 @@ particlesJS("particles-js", {
 /* ---- End particles.js  ---- */
 
 $(document).ready(function() {
-  /* ---- Start nav fixing  ---- */
-  var viewportHeight = "";
-  if (jQuery("window").width() > 1100) {
-    viewportHeight = jQuery("window").height();
-  } else {
-    viewportHeight = jQuery("canvas").height();
-  }
+  var viewportHeight = jQuery("canvas").height();
+  var viewportWidth = $(window).width();
+
   $(window).scroll(function() {
-    if ($(window).scrollTop() > viewportHeight) {
+    if ($(window).scrollTop() > viewportHeight && viewportWidth > 715) {
       $("nav#navbar").removeAttr("id");
       $("nav").addClass("sticky-nav");
     } else {
@@ -62,7 +58,7 @@ $(document).ready(function() {
   });
   /* ---- End nav fixing  ---- */
 
-  /* ---- Start ScrollTop  ---- */
+  /* ---- Start Scroll to Top  ---- */
 
   $("#up").on("click", function() {
     $("html, body").animate(
@@ -72,5 +68,145 @@ $(document).ready(function() {
       2
     );
   });
-  /* ---- End ScrollTop  ---- */
+  /* ---- End Scroll to Top  ---- */
+
+  /* START MENU TOGGLING */
+  $(".menu").on("click", function() {
+    $(".modal-container").css({ display: "none" });
+    $(this).toggleClass("active");
+    $(".overlay").toggleClass("menu-open");
+  });
+  /* END MENU TOGGLING */
+
+  /* START MENU FOR MOBILE */
+  $("nav a").on("click", function() {
+    $(".menu").removeClass("active");
+    $(".overlay").removeClass("menu-open");
+  });
+  /* END MENU FOR MOBILE */
+
+  /* START MODAL */
+  var project = "";
+  $(".link-mod").on("click", function() {
+    project = $(this).attr("id");
+    project = project.substr(project.length - 1);
+    const backdropHeight = $(document).height();
+
+    /* start open modal */
+    $(`div[class='modal-container'][id='modal-p0${project}']`).css({
+      display: "initial"
+    });
+    $("body").css({ overflow: "hidden" });
+    $("#backdrop").css("height", backdropHeight);
+    $("#backdrop").fadeIn(100);
+
+    /* start slider */
+    const carouselSlide = document.querySelector(
+      `#modal-p0${project} .carousel-slide`
+    );
+    const carouselImages = document.querySelectorAll(
+      `#modal-p0${project} .carousel-slide img`
+    );
+    /* end slider */
+
+    /*Buttons */
+    const nextBtn = document.querySelector(`#modal-p0${project} #next-btn`);
+    const prevBtn = document.querySelector(`#modal-p0${project} #prev-btn`);
+    /*Counter */
+    let counter = 1;
+    const size = carouselImages[0].clientWidth;
+
+    carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+
+    /* Button listeners */
+    nextBtn.addEventListener("click", () => {
+      if (counter >= carouselImages.length - 1) return;
+      carouselSlide.style.transition = "transform 0.4s ease-in-out";
+      counter++;
+      carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+    });
+    prevBtn.addEventListener("click", () => {
+      if (counter <= 0) return;
+      carouselSlide.style.transition = "transform 0.4s ease-in-out";
+      counter--;
+      carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+    });
+    carouselSlide.addEventListener("transitionend", () => {
+      if (carouselImages[counter].id === "last-clone") {
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length - 2;
+        carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+      }
+      if (carouselImages[counter].id === "first-clone") {
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length - counter;
+        carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+      }
+    });
+  });
+  /* end open modal */
+
+  /* start close modal through X */
+  $(".modal-close").on("click", function() {
+    $(`#modal-p0${project}`).fadeOut(500);
+    $("body").css({ overflow: "visible" });
+    $(`#backdrop`).fadeOut("slow");
+  });
+  /* end close modal through X */
+
+  /* start close modal through click everywhere */
+  $(document).on("click", function(event) {
+    if (!$(event.target).closest(".modal-container,.link-mod").length) {
+      $(`#modal-p0${project}`).fadeOut(500);
+      $("body").css({ overflow: "visible" });
+      $(`#backdrop`).fadeOut("slow");
+    }
+  });
+  /* end close modal through click everywhere */
+  /* END MODAL */
 });
+
+/* START SLIDER */
+// $(document).ready(function() {
+//   $(".link-mod").on("click", function() {
+//     const carouselSlide = document.querySelector(".carousel-slide");
+//     const carouselImages = document.querySelectorAll(".carousel-slide img");
+
+//     /*Buttons */
+//     const nextBtn = document.querySelector("#next-btn");
+//     const prevBtn = document.querySelector("#prev-btn");
+//     /*Counter */
+//     let counter = 1;
+//     const size = carouselImages[0].clientWidth;
+
+//     carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+
+//     /* Button listeners */
+//     nextBtn.addEventListener("click", () => {
+//       if (counter >= carouselImages.length - 1) return;
+//       carouselSlide.style.transition = "transform 0.4s ease-in-out";
+//       counter++;
+//       carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+//     });
+//     prevBtn.addEventListener("click", () => {
+//       if (counter <= 0) return;
+//       carouselSlide.style.transition = "transform 0.4s ease-in-out";
+//       counter--;
+//       carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+//     });
+//     carouselSlide.addEventListener("transitionend", () => {
+//       if (carouselImages[counter].id === "last-clone") {
+//         carouselSlide.style.transition = "none";
+//         counter = carouselImages.length - 2;
+//         carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+//       }
+//       if (carouselImages[counter].id === "first-clone") {
+//         carouselSlide.style.transition = "none";
+//         counter = carouselImages.length - counter;
+//         carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+//       }
+//     });
+//   });
+// });
+
+/* END SLIDER */
